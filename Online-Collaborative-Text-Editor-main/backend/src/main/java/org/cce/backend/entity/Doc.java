3a8cd080a050d69cfae09290939c86727412631e
Masrur <nabiev.m02004@gmail.com>
2024-12-14 12:44:13 +0300

    initial commit

---
+++ Online-Collaborative-Text-Editor-main/backend/src/main/java/org/cce/backend/entity/Doc.java
@@ -0,0 +1,36 @@
+package org.cce.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Doc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "username")
+    private User owner;
+
+    private String title;
+    @Column(name = "content", columnDefinition = "BYTEA")
+    private byte[] content;
+
+    @OneToMany(mappedBy = "doc")
+    private List<UserDoc> sharedWith;
+
+}
